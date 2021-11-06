727c4200e7f1ea8c95281a0376cde64053f0a24a
Jason Wisniewski <jwizinpa95@gmail.com>
2021-11-06 14:24:27 -0600

    user prompts and second function outline

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,68 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Global variables
+var symbolArray = ["!","@","#","$","^","&","*"];
+var characters = ["letter","number","symbol"];
+var letters = /^[A-Za-z]+$/;
+var numerics = [0,1,2,3,4,5,6,7,8,9];
+
+//password length function varibles made global for later reference 
+var passwordLengthInput
+var specialCharacter
+var upperCaseLetters
+var lowerCaseLetters
+var numerics
+
+// function to ask user what they want and to store info in var above
+var passwordInput = function () {
+
+  // ask user how long they would like their pasword to be
+  passwordLengthInput = window.prompt("How long would you like your password to be enter between 8 and 128 characters");
+  // answer stored in string need to convert to number so we can read it later
 
 
-// ask user how many characters they want the pw to be
+// If player enters in invalid answer bring back to question
+  if (passwordLengthInput < 8 || passwordLengthInput > 128 ||passwordLengthInput === "" || passwordLengthInput === "null") {
+    window.alert("You need to provide a valid answer! Please try again.");
+    return console.log(passwordInput());
+  }
 
-// ask user if they would like special characters 
+  // Would you like special characters? 
+  specialCharacter = window.prompt("Would you like this password to contain special characters? ok = yes cancle = no" ); 
 
-// ask user if they would like numbers
+  // Would you like lower case letters?
+  lowerCaseLetters = window.prompt("Would you like lower case letters? ok = yes cancle = no");
+    console.log(lowerCaseLetters);
 
-// depending on above answers we may need to seperate each into own function
+   // Would you like upper case letters?
+  upperCaseLetters = window.prompt("Would you like upper case letters? ok = yes cancle = no");
 
-// math.rnadom creates number between 0 and 1 randomly use this to create pw?  Or another function?
+    // Would you like numerics?
+  numerics = window.prompt("Would you like numerics? ok = yes cancle = no");
 
-
-
-
-var generatePassword = function () {
-
+  
 };
+passwordInput();
+
+var passwordRandomizer = function(){
+
+  // run through loop amount of times indicated by password length
+  for (var i = 0; i < passwordLengthInput; i++) {
+
+  // if special letter true
+    if (specialCharacter === true) {
+      randomNumber * specialCharacter
+    }
+    // need to store each loop result and add to it as we loop
+  // else no
+
+  // if lower case true 
+  // else no
+
+  // if numeric ture
+  // else false
+  }
+
+}
+passwordRandomizer();
