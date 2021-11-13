813f810f1982266d0dae16745c8c1e09c671e996
Jason Wisniewski <jwizinpa95@gmail.com>
2021-11-12 17:16:05 -0700

    readme completed

---
+++ README.md
@@ -0,0 +1,38 @@
+# <Password Generator>
+## Description
+This project purpose isto make a password generator that asks the user how long they would liek the password to be and if they would like the password to contain special characters, numerics, upper case letters, and lower case letters.  Then produce a password with the user defined chrarcteristics.  
+
+This project taught me about variouse JavaScript funcitons and code orgainzation. 
+
+## Installation
+to install the application clone the code in the repository.  The site is also live at 
+## Usage
+This projecgt can be used by anyone who wants to view skills I have developed in JavaScript.
+
+  <img src="./assets/images/Screenshot.png">
+
+## Credits
+List your collaborators, if any, with links to their GitHub profiles.
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+If you followed tutorials, include links to those here as well.
+## MIT License
+
+Copyright (c) 2021 Jason Wisniewski
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -18,9 +18,7 @@ var characters = ["uppercase","lowercase","number","symbol"];
 var specialCharactersArray = ["!","@","#","$","^","&","*"];
 var lowerCaseLettersArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCaseLettersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var numerics = ["0","1","2","3","4","5","6","7","8","9"];
-var possibleCharacters = [];
-
+var NumericsArray = ["0","1","2","3","4","5","6","7","8","9"];
 
 //password length function varibles made global for later reference 
 var passwordLengthInput
@@ -29,13 +27,13 @@ var upperCaseLetters
 var lowerCaseLetters
 var numerics
 var finalPassword;
+var possibleCharacters = [];
 
 // function to ask user what they want and to store info in var above
 var generatePassword = function () {
-  var possibleCharacters = [];
+  
   // ask user how long they would like their pasword to be, parse int changes answer to number instead of a string
   passwordLengthInput = parseInt(window.prompt("How long would you like your password to be enter between 8 and 128 characters"));
-  // answer stored in string need to convert to number so we can read it later
 
   // If player enters in invalid answer bring back to question
   if (passwordLengthInput < 8 || passwordLengthInput > 128 || !passwordLengthInput) {
@@ -49,31 +47,45 @@ var generatePassword = function () {
 
   //  creating array possibleCharacters to be randomly selected from for 
   if (specialCharacter === true){
-    window.alert("special char true ya ding dong");
-    possibleCharacters.concat(specialCharactersArray);
+    possibleCharacters += specialCharactersArray;
   };
+  console.log('special char', possibleCharacters);
 
   // Would you like lower case letters?
   lowerCaseLetters = window.confirm("Would you like lower case letters? ok = yes cancel = no");
-    console.log(lowerCaseLetters);
 
   if (lowerCaseLetters === true){
-    window.alert("lowercase letters true ya ding dong");
-    possibleCharacters.concat(lowerCaseLettersArray);
+    possibleCharacters +=(lowerCaseLettersArray);
   };
+  console.log('lower case' , possibleCharacters);
 
    // Would you like upper case letters?
   upperCaseLetters = window.confirm("Would you like upper case letters? ok = yes cancel = no");
 
+  if (upperCaseLetters === true){
+    possibleCharacters += upperCaseLettersArray;
+  };
+  console.log('upper case' , possibleCharacters);
+
     // Would you like numerics?
   numerics = window.confirm("Would you like numerics? ok = yes cancle = no");
+
+  if (numerics === true){
+    possibleCharacters += NumericsArray;
+  };
+  console.log('numerics', possibleCharacters);
+
   if(!numerics && !lowerCaseLetters && !upperCaseLetters && !specialCharacter){
   alert("you have to enter at least one valid parameter")
   return ""
   }
   
-
-  // tells OG function to 
+  for (var i = 0; i < passwordLengthInput; i++) {
+    var randomziedCharacter = Math.floor(Math.random * possibleCharacters.length)
+    finalPassword += randomziedCharacter;
+  };
+  console.log('final password', finalPassword);
+  // tells OG function to push into randomizer function
   //return(createPassword(possibleCharacter))
 };
 //  passwordInput();
@@ -82,7 +94,8 @@ var generatePassword = function () {
 //     // run through loop amount of times indicated by password length
 //     for (var i = 0; i < passwordLengthInput; i++) {
 
-//     // then we need to randomly select between each loop
+//     // then we need to randomly selected values from array created above
+
 //     }
 //   }
 //   passwordRandomizer();
